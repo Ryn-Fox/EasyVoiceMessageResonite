9a74cab1877687b4b22debe0c7c8d061d768b4c6
Sox <66733654+Sox-NeosVR@users.noreply.github.com>
2022-07-22 01:56:45 +0100

    Initial commit

---
+++ EasyVoiceMessage/EasyVoiceMessage.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine.UIX;
+using FrooxEngine;
+
+namespace EasyVoiceMessage
+{
+    public class EasyVoiceMessage : NeosMod
+    {
+        public override string Name => "EasyVoiceMessage";
+        public override string Author => "Sox";
+        public override string Version => "1.0.0";
+        public override string Link => "";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.Sox.EasyVoiceMessage");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(FriendsDialog))]
+        class PatchEasyVoiceMessage
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch("OnAttach")]
+            public static void PostFix(FriendsDialog __instance, SyncRef<Button> ____sendVoiceMessageButton)
+            {
+                ____sendVoiceMessageButton.Target.PassThroughVerticalMovement.Value = false;
+                ____sendVoiceMessageButton.Target.PassThroughHorizontalMovement.Value = false;
+            }
+        }
+    }
+}
